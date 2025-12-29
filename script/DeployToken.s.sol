78bd43b4f78ccca3377ec3542061f7d8b2383bef
root <root@DESKTOP-KTDHBRD.localdomain>
2025-12-29 18:02:35 +0800

    feat: additional test

--- script/DeployToken.s.sol
+++ script/DeployToken.s.sol
@@ -5,7 +5,7 @@ import {Script} from "forge-std/Script.sol";
 import {MyToken} from "src/MyToken.sol";
 
 contract DeployToken is Script {
-    uint256 public constant INITIAL_SUPPLY = 1 ether;
+    uint256 public constant INITIAL_SUPPLY = 1000 ether;
     function run() public returns (MyToken) {
         vm.startBroadcast();
         MyToken myToken = new MyToken(INITIAL_SUPPLY);
--- test/Mytoken.t.sol
+++ test/Mytoken.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Inderifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.20;
 
@@ -7,9 +7,35 @@ import {DeployToken} from "script/DeployToken.s.sol";
 import {MyToken} from "src/MyToken.sol";
 
 contract TestMyToken is Test {
-    MyToken mytoken;
-    function setup() public {
-        DeployToken deployToken = new DeployToken();
-        mytoken = deployToken.run();
+    MyToken myToken;
+    DeployToken deployToken;
+
+    address faker = makeAddr("faker");
+    address noah = makeAddr("noah");
+
+    uint256 constant STARTING_BALANCE = 10 ether;
+    function setUp() public {
+        deployToken = new DeployToken();
+        myToken = deployToken.run();
+
+        vm.prank(msg.sender);
+        myToken.transfer(faker, STARTING_BALANCE);
+    }
+
+    function test_fakerBalance() public view {
+        assert(myToken.balanceOf(faker) == STARTING_BALANCE);
+    }
+
+    function test_allowances() public {
+        vm.prank(faker);
+        myToken.approve(noah, STARTING_BALANCE);
+
+        uint256 transferFormBalance = 1 ether;
+        vm.prank(noah);
+        myToken.transferFrom(faker, noah, transferFormBalance);
+
+        assertEq(myToken.balanceOf(noah),transferFormBalance);
+        assertEq(myToken.balanceOf(faker), STARTING_BALANCE - transferFormBalance);
+
     }
 }
